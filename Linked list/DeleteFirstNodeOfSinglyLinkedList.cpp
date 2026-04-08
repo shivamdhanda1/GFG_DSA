4a22214c4987333f2e512d0ac6701350e8ed0160
shivamdhanda1 <shivamdhanda01681@gmail.com>
2026-04-08 23:17:48 +0530

    8th April 3rd commit

---
+++ Linked list/DeleteFirstNodeOfSinglyLinkedList.cpp
@@ -0,0 +1,40 @@
+// Time complexity = O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+int data;
+Node *next;
+Node(int x) {
+  data = x;
+  next = NULL;
+}
+};
+
+Node *delHead(Node *head) {
+  if(head == NULL) {
+    return NULL;
+  }
+  else {
+    Node *temp = head->next;
+    delete head;
+    return temp;
+  }
+}
+
+
+
+int main() {
+  
+  Node *head = new Node(10);
+  head->next = new Node(20);
+  head->next->next = new Node(30);
+  head = delHead(head);
+  while(head != NULL) {
+    cout<<head->data<<" ";
+    head = head->next;
+  }
+  return 0;
+
+}
